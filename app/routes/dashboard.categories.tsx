00cd920eb3add555d5dbec4a69121f71f56329b7
Steeve Matou <steeve.matou@gmail.com>
2023-07-16 11:02:42 +0200

    nothing

--- app/routes/dashboard.categories.tsx
+++ app/routes/dashboard.categories.tsx
@@ -6,21 +6,20 @@ import Table from "~/components/table";
 import { Toast } from "~/components/toast";
 
 export async function loader({ request }: LoaderArgs) {
-    const categories = await category.findAll();
-    return json({ categories });
+  const categories = await category.findAll();
+  //   const categories = await category.findAllRaw();
+  return json({ categories });
 }
 
 export default function CategoryPanel() {
-    const { categories } = useLoaderData();
+  const { categories } = useLoaderData();
 
-    return (
-        <>
-            <Outlet />
-            <div className="overflow-y-scroll flex flex-col gap-y-4 pt-5 ">
-                {categories && (
-                    <Table data={categories} endpoint="/api/categories" />
-                )}
-            </div>
-        </>
-    );
+  return (
+    <>
+      <Outlet />
+      <div className="overflow-y-scroll flex flex-col gap-y-4 pt-5 ">
+        {categories && <Table data={categories} endpoint="/api/categories" />}
+      </div>
+    </>
+  );
 }
