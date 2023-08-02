aabe8ec4b2e77f86b5e4b4ea0bcca3111e722661
Steeve Matou <steeve.matou@gmail.com>
2023-08-02 11:05:19 +0200

    merged old and new repo

--- remix.config.js
+++ remix.config.js
@@ -1,40 +1,4 @@
 /** @type {import('@remix-run/dev').AppConfig} */
-<<<<<<< HEAD
-const { flatRoutes } = require('remix-flat-routes')
-// import { flatRoutes } from 'remix-flat-routes'
-
-module.exports = {
-  ignoredRouteFiles: ["**/.*"],
-  tailwind: true,
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
-  // publicPath: "/build/",
-  serverPlatform: 'node',
-  postcss: true,
-  watchPaths: ['./tailwind.config.ts'],
-  serverModuleFormat: "cjs",
-  future: {
-    v2_headers: true,
-    v2_errorBoundary: true,
-    v2_meta: true,
-    v2_normalizeFormMethod: true,
-    v2_routeConvention: true,
-    // v2_dev: true,
-  },
-  // routes: async defineRoutes => {
-  //   return flatRoutes('routes', defineRoutes)
-  // },
-  routes: async defineRoutes => {
-    return flatRoutes('routes', defineRoutes, {
-      ignoredRouteFiles: [
-        '.*',
-        '**/*.css',
-        '**/*.test.{js,jsx,ts,tsx}',
-        '**/__*.*',
-      ],
-    })
-=======
 const { flatRoutes } = require("remix-flat-routes");
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
@@ -60,6 +24,5 @@ module.exports = {
         "**/__*.*",
       ],
     });
->>>>>>> new_repo/main
   },
 };
